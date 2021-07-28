f92daf2b752df6c4b4c0066e7c4f140da41c4814
6keeper <82912166+6keeper@users.noreply.github.com>
2021-07-28 13:49:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double Pi = Math.PI;
+
+            Console.WriteLine("Please insert radius");
+            var Radius = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please insert heighth");
+            var Heighth = int.Parse(Console.ReadLine());
+
+            var Superf = Pi * Radius * (Radius + Heighth);
+            Console.WriteLine($"Sector equals {Superf}");
+        }
+    }
+}
