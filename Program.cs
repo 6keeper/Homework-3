58285685409359303478d7f041db5b128d2f1572
6keeper <82912166+6keeper@users.noreply.github.com>
2021-07-28 15:03:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 14, y = 1, z = 5;
+            x += y - x++ * z;
+            Console.WriteLine($"X = {x},Y = {y},Z = {z}");
+            z = --x - y * 5;
+            Console.WriteLine($"X = {x},Y = {y},Z = {z}");
+            y /= x + 5 % z;
+            Console.WriteLine($"X = {x},Y = {y},Z = {z}");
+            z = x++ + y * 5;
+            Console.WriteLine($"X = {x},Y = {y},Z = {z}");
+            x = y - x++ * z;
+            Console.WriteLine($"X = {x},Y = {y},Z = {z}");
+        }
+    }
+}
